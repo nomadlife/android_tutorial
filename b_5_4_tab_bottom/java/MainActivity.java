994ef7b9c7ea661507becf08c1e6532813903940
nomadlife_15w <joonwoo3816@gmail.com>
2019-10-29 18:01:47 +0900

    fix b_5_4

--- b_5_4_tab_bottom/java/MainActivity.java
+++ b_5_4_tab_bottom/java/MainActivity.java
@@ -7,8 +7,6 @@ import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-
     Fragment1 fragment1;
     Fragment2 fragment2;
     Fragment3 fragment3;
@@ -18,6 +16,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar)
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.tab1:
+                                Toast.makeText(getApplicationContext(), "첫번째 탭 선택됨",Toast.LENGTH_SHORT).show();
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+                                return true;
+                            case R.id.tab2:
+                                Toast.makeText(getApplicationContext(), "두번째 탭 선택됨", Toast.LENGTH_SHORT).show();
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+                                return true;
+                            case R.id.tab3:
+                                Toast.makeText(getApplicationContext(), "세번째 탭 선택됨", Toast.LENGTH_SHORT).show();
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+                                return true;
+                        }
+                        return false;
+                    }
+                });
     }
 }
