41df52343ec06b657acc98898386147ca031702f
nomadlife_15w <joonwoo3816@gmail.com>
2019-11-01 19:23:17 +0900

    Add Sample RecyclerView_2

---
+++ sample_recyclerview_2/java/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.test_recyclerview_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Movie> dataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.InitializeData();
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        LinearLayoutManager manager = new LinearLayoutManager(this, RecyclerView.VERTICAL,false);
+        recyclerView.setLayoutManager(manager); // LayoutManager 등록
+        recyclerView.setAdapter(new MyAdapter(dataList));  // Adapter 등록
+    }
+
+    public void InitializeData()
+    {
+        dataList = new ArrayList<>();
+
+        dataList.add(new Movie(R.drawable.movie1,"어벤져스", "12세관람가"));
+        dataList.add(new Movie(R.drawable.movie2,"미션임파서블", "15세관람가"));
+        dataList.add(new Movie(R.drawable.movie3,"아쿠아맨", "12세관람가"));
+
+        dataList.add(new Movie(R.drawable.movie4,"범죄도시", "15세관람가"));
+        dataList.add(new Movie(R.drawable.movie5,"공공의적", "15세관람가"));
+        dataList.add(new Movie(R.drawable.movie6,"맨인블랙", "15세관람가"));
+
+        dataList.add(new Movie(R.drawable.movie7,"고질라", "12세관람가"));
+        dataList.add(new Movie(R.drawable.movie8,"캡틴마블", "12세관람가"));
+        dataList.add(new Movie(R.drawable.movie9,"아이언맨", "12세관람가"));
+    }
+}
