6bdb6d7cb62cb91e000642ef9ec7dfc8dc59087b
nomadlife_15w <joonwoo3816@gmail.com>
2019-11-04 21:46:38 +0900

    Add viewpager_step2

---
+++ test_viewpager_step2/java/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.test_viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager());
+        // ViewPager와  FragmentAdapter 연결
+        viewPager.setAdapter(fragmentAdapter);
+
+        ArrayList<Integer> listImage = new ArrayList<>();
+        listImage.add(R.drawable.lighthouse);
+        listImage.add(R.drawable.penguins);
+        listImage.add(R.drawable.tulips);
+
+        viewPager.setClipToPadding(false);
+        int dpValue = 16;
+        float d = getResources().getDisplayMetrics().density;
+        int margin = (int) (dpValue * d);
+        viewPager.setPadding(margin, 0, margin, 0);
+        viewPager.setPageMargin(margin/2);
+
+        // FragmentAdapter에 Fragment 추가, Image 개수만큼 추가
+        for (int i = 0; i < listImage.size(); i++) {
+            ImageFragment imageFragment = new ImageFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("imgRes", listImage.get(i));
+            imageFragment.setArguments(bundle);
+            fragmentAdapter.addItem(imageFragment);
+        }
+        fragmentAdapter.notifyDataSetChanged();
+
+    }
+
+
+    class FragmentAdapter extends FragmentPagerAdapter {
+
+        // ViewPager에 들어갈 Fragment들을 담을 리스트
+        private ArrayList<Fragment> fragments = new ArrayList<>();
+
+        // 필수 생성자
+        FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        // List에 Fragment를 담을 함수
+        void addItem(Fragment fragment) {
+            fragments.add(fragment);
+        }
+
+
+    }
+
+
+}
