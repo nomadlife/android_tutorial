976f719488a0b6271904628660d1ae5f7317313a
noamdlife15u <joonwoo3816@gmail.com>
2019-11-05 12:59:03 +0900

    change library x -> support

--- sample_recyclerview/java/HorizontalAdapter.java
+++ sample_recyclerview/java/HorizontalAdapter.java
@@ -6,8 +6,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
